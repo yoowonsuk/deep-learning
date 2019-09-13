cd2db52600982ebafaf41c26de1bfde9191d4c98
yoowonsuk <sam2yuk@naver.com>
2019-09-13 10:57:44 +0900

    13th Sep 2019

--- ch1/2.py
+++ ch1/2.py
@@ -3,6 +3,6 @@ import numpy as np
 A = np.array([[1, 2], [3, 4]])
 print(A)
 
-A.shape
+print(A.shape)
 
-A.dtype
+print(A.dtype)
--- ch1/3.py
+++ ch1/3.py
@@ -1,8 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-x = np.arange(0, 6, 0.1)
-y = np.sin(x)
+X = np.array([[51, 55], [14, 19], [0, 4]])
 
-plt.plot(x, y)
-plt.show()
+print(X)
+
+print(X[0])
+
+print(X[0][1])
+
+for row in X:
+    print(row)
+
+X = X.flatten()
+print(X)
+
+print(X[np.array([0, 2, 4])])
+
+print(X > 15)
+
+print(X[X>15])
