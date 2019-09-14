f749efb4686f0057b33250d4a9555492ea21edcd
yoowonsuk <sam2yuk@naver.com>
2019-09-14 11:51:56 +0900

    14th Sep 2019

--- ch2/1.py
+++ ch2/1.py
@@ -1,10 +1,14 @@
-import numpy as np
-x = np.array([0, 1])
-w = np.array([0.5, 0.5])
-b = -0.7
+def AND(x1, x2):
+    w1, w2, theta = .5, .5, .7
+    tmp = x1*w1 + x2*w2
 
-print(w*x)
+    if tmp <= theta:
+        return 0
+    else:
+        return 1
 
-print(np.sum(w*x))
 
-print(np.sum(w*x)+b)
+print(AND(0, 0))
+print(AND(1, 0))
+print(AND(0, 1))
+print(AND(1, 1))
